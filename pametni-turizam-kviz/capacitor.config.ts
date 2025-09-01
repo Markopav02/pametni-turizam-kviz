1a0984f8b0ba14bb0e4cd7233ad3134c2dae4c0c
Marko Pavlovic <markopa002@gmail.com>
2025-09-01 11:16:48 +0200

    Napravljen pocetni projekat

---
+++ pametni-turizam-kviz/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pametni-turizam-kviz',
+  webDir: 'www'
+};
+
+export default config;
